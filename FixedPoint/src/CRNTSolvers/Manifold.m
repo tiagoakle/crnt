fc74a5d4a3c0b4972fe68e9b8fbe74126544e645
tiagoakle <tiagoakle@gmail.com>
2012-10-17 13:53:51 -0700

    Fixed typo in Ronan's last name

--- FixedPoint/src/CRNTSolvers/Manifold.m
+++ FixedPoint/src/CRNTSolvers/Manifold.m
@@ -3,29 +3,27 @@
 
 
 %Set the number of complexes to n and of species to m
-%{
-m = 5;
-n = 50;
-r = 3;
+
+m = 500;
+n = 5000;
+r = 30;
 
 cd ../Generator
 Y = YGenerator(m,n,r);
-A = AkGenerator(n,0.2,1);
+A = AkGenerator(n,0.3,3);
 cd ../CRNTSolvers
-%}
+
 
 %Requires Y,Ak.
 [m,n] = size(Y);
 
-%XXX
-%{
+
 %Form an inhomogeneous term
 gamm = randn(n,1);
 iota = Y*A*gamm;
-%}
 
-%XXX
-d        = - diag(Ak);
+
+d        = - diag(A);
 At       = A + diag(d);
 eta      = abs(randn(n,1));
 s        = 0.1*ones(n,1);
@@ -33,13 +31,14 @@ s        = 0.1*ones(n,1);
 etaPlus  = eta + s;
 etaMinus = eta + diag(1./d)*At*s;
 iota     = Y*(At*etaPlus-d.*etaMinus);
-
+n_iota   = norm(iota);
 
 eta   = ones(m,1); %initial log concentrations
 rho   = exp(Y'*eta);
 
-alpha  = 0.1;
-lambda = 1e-8;
+alpha  = 1;
+delta = 1e-8;
+%delta = 0;
 assert(size(A,1) == n);
 iter = 0;
 
@@ -47,9 +46,13 @@ norms = [];
 done = false;
 while ~done
     %Find the tangent space to the manifold of complex fluxes.
-    R   = diag(rho);
+    RHO   = diag(rho);
     rhs = iota-Y*A*rho; 
-    nu  = (Y*A*R*Y'+lambda*eye(m))\(rhs);
+    %nu  = (Y*A*RHO*Y'+delta*eye(m))\(rhs);
+    [Q,R,E] = qr(Y*A*RHO*Y');
+    R(m,m) = 1; 
+    nu   = R\(Q'*rhs);
+    nu   = E*nu;
     %Backtracking search
    % alpha = 1;
    % bt  = true;
@@ -64,8 +67,9 @@ while ~done
     fprintf('\n Norm at iteration %i : %d, norm nu %d, norm eta %d, norm rho %d ', iter,nor,norm(nu),norm(eta),norm(rho));
     iter = iter + 1;
     norms = [norms,nor];
-    if(iter > 1000) 
+    if(nor/n_iota<1e-10) 
         done = true;
     end
 end
-plot(log(norms));
+fprintf('\n');
+plot(log10(norms));
